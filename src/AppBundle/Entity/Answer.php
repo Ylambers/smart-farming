9cecd30b1fef9cd8e4366ab9a107c71d8df88319
Rianne L <riannelubberink@gmail.com>
2018-12-13 12:31:53 +0100

    Answer and Company table

---
+++ src/AppBundle/Entity/Answer.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table(name="answer")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AnswerRepository")
+ */
+class Answer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="media_path", type="string", length=255, nullable=true)
+     */
+    private $mediaPath;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="likes", type="integer", nullable=true)
+     */
+    private $likes;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="dislikes", type="integer", nullable=true)
+     */
+    private $dislikes;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_posted", type="datetime")
+     */
+    private $datePosted;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="id")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user_id;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Answer
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set mediaPath
+     *
+     * @param string $mediaPath
+     *
+     * @return Answer
+     */
+    public function setMediaPath($mediaPath)
+    {
+        $this->mediaPath = $mediaPath;
+
+        return $this;
+    }
+
+    /**
+     * Get mediaPath
+     *
+     * @return string
+     */
+    public function getMediaPath()
+    {
+        return $this->mediaPath;
+    }
+
+    /**
+     * Set likes
+     *
+     * @param integer $likes
+     *
+     * @return Answer
+     */
+    public function setLikes($likes)
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    /**
+     * Get likes
+     *
+     * @return int
+     */
+    public function getLikes()
+    {
+        return $this->likes;
+    }
+
+    /**
+     * Set dislikes
+     *
+     * @param integer $dislikes
+     *
+     * @return Answer
+     */
+    public function setDislikes($dislikes)
+    {
+        $this->dislikes = $dislikes;
+
+        return $this;
+    }
+
+    /**
+     * Get dislikes
+     *
+     * @return int
+     */
+    public function getDislikes()
+    {
+        return $this->dislikes;
+    }
+
+    /**
+     * Set datePosted
+     *
+     * @param \DateTime $datePosted
+     *
+     * @return Answer
+     */
+    public function setDatePosted($datePosted)
+    {
+        $this->datePosted = $datePosted;
+
+        return $this;
+    }
+
+    /**
+     * Get datePosted
+     *
+     * @return \DateTime
+     */
+    public function getDatePosted()
+    {
+        return $this->datePosted;
+    }
+}
+
