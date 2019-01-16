8c3e6a58510ed0e9a30156d12fd3c1225abd0807
Yaron Lambers <yaronlambers@gmail.com>
2019-01-16 11:34:18 +0100

    fix

--- src/AppBundle/Form/TopicType.php
+++ src/AppBundle/Form/TopicType.php
@@ -7,6 +7,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
@@ -18,14 +20,19 @@ class TopicType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-        $builder->add('title');
-           $builder ->add('topic_type', ChoiceType::class, [
-                'choices' => TopicTypeEnum::getTopicTypes(),
-                'choice_label' => function($choice){
-                    return TopicTypeEnum::getTopicTypeName($choice);
-                }
-            ]);
-        $builder->add('text');
+        $builder->add('title', TextType::class, [
+            'label' => "Titel"
+        ]);
+       $builder ->add('topic_type', ChoiceType::class, [
+            'choices' => TopicTypeEnum::getTopicTypes(),
+            'choice_label' => function($choice){
+                return TopicTypeEnum::getTopicTypeName($choice);
+            }
+        ]);
+        $builder->add('text', TextareaType::class, [
+            'label' => "Voer uw topic vraag in",
+            'placeholder' => "Hoi"
+        ]);
         $builder->add("media_path", FileType::class);
         $builder->add('solved');
         if($this->authorization->isGranted('ROLE_SUPER_ADMIN'))
